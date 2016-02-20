df277250ba112d76e8ceaf4448540725f9e63a06
xonix <xonixx@gmail.com>
2016-02-20 16:48:15 +0200

    more work on edit

--- src/main/webapp/add.jsp
+++ src/main/webapp/add.jsp
@@ -1,38 +1,40 @@
 <div class="container">
     <h3 class="text-center">{{isEdit ? 'Edit' : 'Add'}}</h3>
-    <form class="form-horizontal" autocomplete="off">
+    <form name="passForm" class="form-horizontal" novalidate autocomplete="off">
         <div style="height: 0;width: 0;overflow: hidden"><%-- hack against chrome autocomplete --%>
-            <input style="" type="text" name="fakeusername"/>
-            <input style="" type="password" name="fakepassword"/>
+            <input type="text" name="fakeusername"/>
+            <input type="password" name="fakepassword"/>
         </div>
         <div class="form-group">
             <label for="inputUrl" class="col-sm-2 control-label">URL</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputUrl" placeholder="URL">
+                <input ng-model="password.url" type="text" class="form-control" id="inputUrl" placeholder="URL">
             </div>
         </div>
         <div class="form-group">
-            <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+            <label for="inputLogin" class="col-sm-2 control-label">Login</label>
             <div class="col-sm-10">
-                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                <input ng-model="password.login" type="text" class="form-control" id="inputLogin" placeholder="Login">
             </div>
         </div>
         <div class="form-group">
             <label for="inputDescr" class="col-sm-2 control-label">Description</label>
             <div class="col-sm-10">
-                <textarea class="form-control" id="inputDescr" placeholder="Description"></textarea>
+                <textarea ng-model="password.descr" class="form-control" id="inputDescr"
+                          placeholder="Description"></textarea>
             </div>
         </div>
         <div class="form-group">
             <label for="inputPass" class="col-sm-2 control-label">Password</label>
             <div class="col-sm-10">
-                <input type="password" pass="" class="form-control" id="inputPass" placeholder="Password">
+                <input ng-model="password.pass" type="password" pass="" class="form-control" id="inputPass"
+                       placeholder="Password">
             </div>
         </div>
         <div class="form-group">
             <label for="inputTags" class="col-sm-2 control-label">Tags</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputTags" placeholder="Tags">
+                <input ng-model="password.tags" type="text" class="form-control" id="inputTags" placeholder="Tags">
             </div>
         </div>
         <div class="form-group">
--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -3,7 +3,7 @@ angular.module('pass-manager', ['ngRoute'])
         $routeProvider
             .when('/list', {templateUrl: 'list.jsp', controller: 'ListCtrl'})
             .when('/add', {templateUrl: 'add.jsp', controller: 'AddCtrl'})
-            .when('/edit', {templateUrl: 'add.jsp', controller: 'AddCtrl', edit: 1})
+            .when('/edit/:uid', {templateUrl: 'add.jsp', controller: 'AddCtrl', edit: 1})
             .otherwise({redirectTo: '/list'});
     }])
     .directive('pass', [function () {
@@ -15,23 +15,71 @@ angular.module('pass-manager', ['ngRoute'])
             });
         }
     }])
-    .controller('ListCtrl', ['$scope', function ListCtrl($scope) {
-        $scope.passwords = [
-            {tags:['tag1', 'tag2'], descr: 'Some long detailed description 123', url:'https://google.com', pass: "password1"},
-            {tags:['tag1', 'tag2', 'tag3'], descr: 'Some other long description 123', url:'https://google.com', pass: "password1"},
-            {tags:['tag3'], descr: 'dolor sit amet', url:'https://apple.com', pass: "password2"}
+    .factory('PasswordsFunctions', [function () {
+        var initial = [
+            {
+                uid: newUid(),
+                tags: ['tag1', 'tag2'],
+                descr: 'Some long detailed description 123',
+                url: 'https://google.com',
+                login: 'aaa@bbb.ccc',
+                pass: "password1"
+            },
+            {
+                uid: newUid(),
+                tags: ['tag1', 'tag2', 'tag3'],
+                descr: 'Some other long description 123',
+                url: 'https://google.com',
+                login: 'login',
+                pass: "password1"
+            },
+            {
+                uid: newUid(),
+                tags: ['tag3'],
+                descr: 'dolor sit amet',
+                url: 'https://apple.com',
+                login: 'login2',
+                pass: "password2"
+            }
         ];
+        return {
+            passwords: initial,
+            getCurrentList: function () {
+                return this.passwords;
+            },
+            add: function (password) {
+                this.passwords.push(password);
+            },
+            getByUid: function (uid) {
+                for (var i = 0; i < this.passwords.length; i++) {
+                    var p = this.passwords[i];
+                    if (p.uid == uid)
+                        return p;
+                }
+                return null;
+            }
+        }
+    }])
+    .controller('ListCtrl', ['$scope', 'PasswordsFunctions', function ListCtrl($scope, PasswordsFunctions) {
+        $scope.passwords = PasswordsFunctions.getCurrentList();
 
         $scope.addNew = function () {
             location.href = '#/add';
         };
         $scope.edit = function (p) {
-            location.href = '#/edit';
+            location.href = '#/edit/' + p.uid;
         };
     }])
-    .controller('AddCtrl', ['$scope', '$route', function AddCtrl($scope, $route) {
-        $scope.isEdit = $route.current.$$route.edit;
-        $scope.cancel = function () {
-            location.href = '#/list';
-        }
-    }]);
+    .controller('AddCtrl', ['$scope', '$routeParams', 'PasswordsFunctions',
+        function AddCtrl($scope, $routeParams, PasswordsFunctions) {
+            var uid = $routeParams.uid;
+            $scope.isEdit = !!uid;
+            $scope.password = uid ? PasswordsFunctions.getByUid(uid) : {};
+            $scope.cancel = function () {
+                location.href = '#/list';
+            }
+        }]);
+
+function newUid() {
+    return '' + new Date().getTime() + '-' + Math.floor(Math.random() * 1e10);
+}
