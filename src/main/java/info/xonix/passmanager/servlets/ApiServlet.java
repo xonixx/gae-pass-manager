205ec0d5a47b9eb83eb2b34ce7b6cda35322761a
xonix <xonixx@gmail.com>
2016-03-05 18:38:38 +0300

    server logic - working on backups

--- src/main/java/info/xonix/passmanager/servlets/ApiServlet.java
+++ src/main/java/info/xonix/passmanager/servlets/ApiServlet.java
@@ -1,6 +1,9 @@
 package info.xonix.passmanager.servlets;
 
-import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Text;
 import info.xonix.passmanager.Logic;
 
 import javax.servlet.ServletException;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -21,7 +25,19 @@ import java.util.logging.Logger;
 public class ApiServlet extends HttpServlet {
     private static final Logger log = Logger.getLogger(ApiServlet.class.getName());
 
+    public static final int BACKUPS_COUNT = 10;
+
+    public static final String KEY_MASTER_DATA = "0";
+    public static final String ENTITY_DATA = "Data";
+    public static final String ENTITY_DATA_BACKUP = "DataBackup";
+
     public static final String ACTION = "action";
+    public static final String ACTION_LOAD = "load";
+    public static final String ACTION_SAVE = "save";
+
+    public static final String PROP_VALUE = "value";
+    public static final String PROP_TIMESTAMP = "timestamp";
+    public static final String PROP_LAST_BACKUP_KEY = "lastBackupKey";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,15 +45,16 @@ public class ApiServlet extends HttpServlet {
 
         Entity data = null;
         try {
-            data = Logic.getDatastoreService().get(KeyFactory.createKey("Data", "1"));
+            data = Logic.getDatastoreService().get(KeyFactory.createKey(ENTITY_DATA, KEY_MASTER_DATA));
         } catch (EntityNotFoundException e) {
             log.info("Data not found");
         }
 
         resp.setContentType("application/json");
-        if ("load".equals(action)) {
+
+        if (ACTION_LOAD.equals(action)) {
             Map<String, String> res = new LinkedHashMap<>();
-            String value = data != null ? ((Text) data.getProperty("value")).getValue() : null;
+            String value = data != null ? ((Text) data.getProperty(PROP_VALUE)).getValue() : null;
 
             log.info("Found data: " + value);
 
@@ -56,10 +73,34 @@ public class ApiServlet extends HttpServlet {
 
         resp.setContentType("application/json");
 
-        if ("save".equals(action)) {
-            Entity data = new Entity("Data", "1");
+        if (ACTION_SAVE.equals(action)) {
+            Entity currentData = null;
+            try {
+                currentData = Logic.getDatastoreService().get(KeyFactory.createKey(ENTITY_DATA, KEY_MASTER_DATA));
+            } catch (EntityNotFoundException e) {
+                log.info("Data not found");
+            }
+
+            int newBackupKey = -1; // no backup
+            if (currentData != null) {
+                Integer lastBackupKey = (Integer) currentData.getProperty(PROP_LAST_BACKUP_KEY);
+                if (lastBackupKey != null) {
+                    newBackupKey = (lastBackupKey + 1) % BACKUPS_COUNT;
+                }
+
+                log.info("Storing backup: #" + newBackupKey);
+                Entity backupData = new Entity(ENTITY_DATA_BACKUP, "" + newBackupKey);
+                backupData.setProperty(PROP_VALUE, currentData.getProperty(PROP_VALUE));
+                backupData.setProperty(PROP_TIMESTAMP, new Date());
+                backupData.setProperty(PROP_LAST_BACKUP_KEY, newBackupKey);
+            }
+
+
+            Entity data = new Entity(ENTITY_DATA, KEY_MASTER_DATA);
             String val = (String) json.get("data");
-            data.setProperty("value", new Text(val));
+            data.setProperty(PROP_VALUE, new Text(val));
+            data.setProperty(PROP_TIMESTAMP, new Date());
+            data.setProperty(PROP_LAST_BACKUP_KEY, newBackupKey);
 
             log.info("Storing data: " + val);
 
