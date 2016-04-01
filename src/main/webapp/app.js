2dc4d62f0d4ad98b23ce8afd36d90b062b2410c3
xonix <xonixx@gmail.com>
2016-04-02 01:03:12 +0300

    fmt file sizes

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -85,6 +85,15 @@ angular.module('pass-manager', ['ngRoute', 'ngResource', 'ngTagsInput'])
             return dateF(input, 'd MMM yyyy HH:mm');
         }
     }])
+    .filter('bytes', function() {
+        return function(bytes, precision) {
+            if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+            if (typeof precision === 'undefined') precision = 1;
+            var units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'],
+                number = Math.floor(Math.log(bytes) / Math.log(1024));
+            return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+        }
+    })
     .filter('to_trusted', ['$sce', function($sce){
         return function(text) {
             return $sce.trustAsHtml(text);
