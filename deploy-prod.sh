1739f4461bdb5a62e828f4a64c46f94b0b1d02be
xonix <xonixx@gmail.com>
2020-05-05 00:33:16 +0300

    Deployments using appcfg are no longer supported #16

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-mvn clean appengine:update -Pprod -Dappid=xonixpwd
+mvn clean package appengine:deploy -Pprod -Dapp.deploy.projectId=xonixpwd
